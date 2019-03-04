73d7a2c36d902948394438d2671ba07dacc10274
lszwaba <lukasz.lool@gmail.com>
2019-03-04 19:41:46 +0100

    add class Book.cs and BookStore.cs

---
+++ ProjektBook/ProjektBook/Model/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektBook.Model
+{
+    class Book
+    {
+        public String titleBook {
+            get;
+            set;
+        }
+
+        public String Author {
+            get;
+            set;
+        }
+
+        public long ID
+        {
+            get;
+            set;
+        }
+    }
+
+}
---
+++ ProjektBook/ProjektBook/Model/BookStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektBook.Model
+{
+    class BookStore
+    {
+        public String Name
+        {
+            get;
+            set;
+        }
+
+        public String Adress
+        {
+            get;
+            set;
+        }
+
+        public long ID
+        {
+            get;
+            set;
+        }
+        public List<Book> Books {
+            get;
+            set; }
+
+    }
+}
