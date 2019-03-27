70dfa988c12eeb6fc969557bf54353c25da7decb
lszwaba <lukasz.lool@gmail.com>
2019-03-27 18:09:31 +0100

    add loop while in BookManagmentServices.cs

--- ProjektBook/ProjektBook/Service/BookManagmentServices.cs
+++ ProjektBook/ProjektBook/Service/BookManagmentServices.cs
@@ -25,6 +25,37 @@ namespace ProjektBook.Service
                 booklist.Add(bookToAdd);
             }
         }
+        public void ExecuteWhile()
+        {
+            var booklist = new List<Book>();
+            int indeks = 0;
+            while (indeks <= 10)
+            {
+                Book bookToAdd = new Book();
+                bookToAdd.ID = indeks;
+                bookToAdd.titleBook = "title_" + indeks.ToString();
+                bookToAdd.Author = "Ziomek";
+                booklist.Add(bookToAdd);
+                indeks++;
+            }
+        }
+        public void ExecuteDoWhile()
+        {
+            var booklist = new List<Book>();
+            int indeks = 0;
+            do
+            {
+                Book bookToAdd = new Book();
+                bookToAdd.ID = indeks;
+                bookToAdd.titleBook = "title_" + indeks.ToString();
+                bookToAdd.Author = "Ziomek";
+                booklist.Add(bookToAdd);
+                indeks++;
+            }
+            while{
+
+            }
+        }
 
     }
 }
